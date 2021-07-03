7513183fa5247c84d1089d8365d1d582cbb4652b
AizenAngel <boza.mitrovic@gmail.com>
2021-07-03 15:24:25 +0200

    Add info_content, get_length_dist

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import fsync
 from pandas import read_csv
 from nltk.corpus import wordnet as wn
 from nltk.corpus import brown
@@ -14,7 +15,26 @@ from sklearn.metrics import accuracy_score
 
 class Main:
     def __init__(self):
-        pass
+        self.ALPHA = 0.2
+        self.BETA = 0.45
+        self.ETA = 0.4
+        self.PHI = 0.2
+        self.DELTA = 0.85
+
+        self.info_content()
+    
+
+    def info_content(self):
+        self.N = 0
+        self.brown_freqs = {}
+
+        for sent in brown.sents:
+            for word in sent:
+                word = word.lower()
+                if word not in self.brown_freqs:
+                    self.brown_freqs[word] = 1 # TODO  Was 0
+                self.brown_freqs[word] = self.brown_freqs[word] + 1
+                self.N += 1
     
     def get_best_synset_pair(self, word1, word2):
         synsets_word1 = wn.synsets(word1)
@@ -37,10 +57,45 @@ class Main:
         return best_synsets
 
 
+    def get_length_dist(self, synset_1, synset_2):
+
+        if synset_1 is None or synset_2 is None:
+            return 0.0
+        if synset_1 == synset_2:
+            return 1
+        
+        wset1 = set([str(x.name) for x in synset_1.lemmas()])
+        wset2 = set([str(x.name) for x in synset_2.lemmas()])
+
+        if len(wset1.intersection(wset2)) > 0:
+            return np.exp(-1 * self.ALPHA)
+        
+        dist = synset_1.shortest_path_distance(synset_2)
+
+        if dist is None:
+            return 1
+
+        return np.exp(-dist * self.ALPHA)
+
+
+    def hierarchy_dist(self):
+        pass
+
+    def most_similar_words(self):
+        pass
+
+    
 
 if __name__ == "__main__":
     main = Main()
-    print(f"BEST PAIR: {main.get_best_synset_pair('RAM', 'memory')}")
+    # print(f"BEST PAIR: {main.get_best_synset_pair('RAM', 'memory')}")
 
+    # synsets_word1 = wn.synsets("RAM")
+    # print(synsets_word1[0])
+    # print( set([str(x.name()) for x in synsets_word1[0].lemmas()]))
 
+    synsets_word1 = wn.synsets("RAM")
+    fsynset = synsets_word1[0]
+    ssynset = synsets_word1[1]
 
+    print(fsynset.shortest_path_distance(ssynset))
